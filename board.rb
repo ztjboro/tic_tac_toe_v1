0eb4c93e504fd45ac162b87e9ea342ae96439196
Pawel Borawski <ztjboro@gmail.com>
2021-06-22 20:35:03 -0400

    added #empty? and #valid? methods

--- board.rb
+++ board.rb
@@ -3,5 +3,33 @@ class Board
     def initialize
         @board = Array.new(3) { Array.new(3){"_"} }
     end
+
+    def valid?(position)
+        if self.empty?(position)
+            return true
+        else 
+            return false
+        end
+    end
+
+    def empty?(position)
+        x = position[0]
+        y = position[1]
+        if @board[x-1][y-1] == "_"
+            return true
+        else 
+            return false
+        end
+    end
+
+    # def place_mark(position, mark)
+
+    #     if @board.valid?(position) && @board.empty?(position)
+    #         @board[position] = mark
+    #     else
+    #         puts "position not valid"
+    #     end
+
+    # end
     
 end
