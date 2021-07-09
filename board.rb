3c11e43eb95c20ad79f8a615dbe563707a9c2360
Pawel Borawski <ztjboro@gmail.com>
2021-07-09 17:12:42 -0400

    added .place_mark method

--- board.rb
+++ board.rb
@@ -24,5 +24,21 @@ class Board
         end
     end
 
+    def place_mark(position, mark)
+        x = position[0] - 1
+        y = position[1] - 1
+        if self.empty?(position)
+          @grid[x][y] = mark
+          return mark
+        elsif !self.valid?(position)
+            return "Position invalid"
+        elsif !self.empty?(position)
+            return "Position taken"
+        end  
+    end
+
+    def print
+    end
+
     
 end
